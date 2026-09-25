4cc8b867a36f2c0696850572e1b1741d02de9203
YourSmurfPapa <nikola.sosova@student.ukf.sk>
2026-09-25 14:56:10 +0200

    02_filtrovanie podla hodnoty

--- Seminar1_1a/02_filtrovanie_podla_hodnoty.sql
+++ Seminar1_1a/02_filtrovanie_podla_hodnoty.sql
@@ -2,4 +2,4 @@ SELECT o.order_id, c.customer_name, o.sales 
 FROM orders o
 INNER JOIN customers c ON o.customer_id = c.customer_id 
 WHERE o.sales > 500 
-ORDER BY o.sales DESC;
+ORDER BY o.sales desc;
